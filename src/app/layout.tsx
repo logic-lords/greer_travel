0cd2bbf940fd2b04e63abc8ecd24642ff6009191
miharyjoe <miharyjoel@gmail.com>
2024-08-17 02:25:25 +0300

    refactor: navbar2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import NavBar from "./home/components/navBar";
 import { NavBarMenu } from "@/components/NavBarMenu";
 
 
